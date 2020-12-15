c992f2a7796e14e91ae8314b46dbe0eaffc7ac39
Doniyorbek <doniyorbek.mashrabboyev@gmail.com>
2020-12-15 14:27:15 +0500

    first commit

---
+++ SwiftUI-HelloWorld/SwiftUI_HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HelloWorldApp.swift
+//  SwiftUI-HelloWorld
+//
+//  Created by Doniyorbek on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
